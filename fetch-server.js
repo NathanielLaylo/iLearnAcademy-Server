39c65bd93232473aeb9fe2dffdb70d69390935ee
NathanielLaylo <91972178+NathanielLaylo@users.noreply.github.com>
2023-02-02 22:54:45 +0000

    initial
    initial commit

---
+++ fetch-server.js
@@ -0,0 +1,134 @@
+const express = require("express");
+const cors = require("cors");
+var path = require("path");
+var morgan = require("morgan");
+
+
+let propertiesReader = require("properties-reader");
+let propertiesPath = path.resolve(__dirname, "conf/db.properties");
+let properties = propertiesReader(propertiesPath);
+
+let dbPprefix = properties.get("db.prefix");
+let dbUsername = encodeURIComponent(properties.get("db.user"));
+let dbPwd = encodeURIComponent(properties.get("db.pwd"));
+let dbName = properties.get("db.dbName");
+let dbUrl = properties.get("db.dbUrl");
+let dbParams = properties.get("db.params");
+
+const uri = dbPprefix + dbUsername + ":" + dbPwd + dbUrl + dbParams;
+
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+let db = client.db(dbName);
+
+let app = express();
+app.set('json spaces', 3);
+app.use(cors());
+//middleware logger
+app.use(morgan("short"));
+
+app.use(express.json());
+
+app.get("/", function(req, res){
+  /*  // console.log(db.collection("lessons").find({}).toArray());
+    req.collection = db.collection("lessons");
+   // console.log(req.collection.find({}).toArray())
+
+    db.collection('lessons').find({}).toArray()
+    .then(function(lessons) {
+      //console.log(lessons); // Use this to debug
+     // callback(lessons);
+    }) */
+
+    res.send("select collection1");
+    
+}); 
+
+app.param('collectionName', function (req, res, next, collectionName) {
+        req.collection = db.collection(collectionName);
+        return next();
+    });
+
+/* app.get('/collections/:collectionName', function (req, res, next) {
+    req.collection.find({}).toArray().then(function (results, error) {
+        if(error){
+            return next(error);
+        }
+        res.send(results);
+    });
+});
+ */
+
+//-1 descending 1 ascending
+app.get('/collections/:collectionName', function (req, res, next) {
+    req.collection.find({}, {sort: [["price", 1]]}).toArray().then(function (results, error) {
+        if(error){
+            return next(error);
+        }
+        res.send(results);
+    });
+});
+
+
+  
+
+/* app.get('/collections/:collectionName', function (req, res, next) {
+        req.collection.find({}).toArray().then(function (err, results) {
+            if (err) {
+                return next(err);
+            }
+            res.send(results);
+        });
+    }); */
+
+app.post('/collections/:collectionName', function (req, res, next){
+    req.collection.insertOne(req.body).then(function (results, error) {
+        if(error){
+            return next(error);
+        }
+        res.send(results);
+    });
+});
+
+
+app.delete('/collections/:collectionName/:id', function (req, res, next){
+    req.collection.deleteOne({_id: new ObjectId(req.params.id)}).then(function(result, err)
+     {
+        if(err){
+            return next(err);
+        }
+        res.send((result.deletedCount === 1) ? {msg: "success"} : {msg: "error"});
+    });
+});
+
+
+
+app.put('/collections/:collectionName/:id', function (req, res, next) {
+        // TODO: Validate req.body
+        req.collection.updateOne({ _id: new ObjectId(req.params.id) },
+            { $set: req.body },
+            { safe: true, multi: false }).then(function (result, err) {
+                if (err) {
+                    return next(err);
+                } else {
+                    res.send((result.matchedCount === 1) ? { msg: "success" } : { msg: "error" });
+                }
+            }
+        );
+    });
+
+
+
+
+
+
+
+app.use(function(req, res){
+    res.status(404).send("Resource not found");
+});
+
+app.listen(3000, function(){
+    console.log("app started on port 3000");
+});
