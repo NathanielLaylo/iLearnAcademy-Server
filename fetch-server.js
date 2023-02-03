5a79139921304fe40f85b819594eea5e2c0cb2a2
NathanielLaylo <laylonathan1@yahoo.com>
2023-02-03 16:56:15 +0000

    Update fetch-server.js

--- fetch-server.js
+++ fetch-server.js
@@ -58,7 +58,7 @@ app.get('/collections/:collectionName/:query', function (req, res, next) {
     let re = new RegExp(`/${query}/i`);
 
 
-    req.collection.find({subject:  new RegExp(query + 'i') }).toArray().then(function (results, error) {
+    req.collection.find({subject:  new RegExp(query, 'i') }).toArray().then(function (results, error) {
         if(error){
             return next(error);
         }
